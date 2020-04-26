83f5f37f3d50f0d35189b94bceb8ea4a684493f7
Luciano Matsuo <matsuoluciano@gmail.com>
2020-04-26 15:45:14 -0300

    agregando navbar y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,11 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// importar rutas
+import { ROUTES } from './app.routes';
+
+
 
 @NgModule({
   declarations: [
@@ -17,6 +23,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
+    RouterModule.forRoot(ROUTES, { useHash: true }),
     BrowserModule,
     AppRoutingModule
   ],
